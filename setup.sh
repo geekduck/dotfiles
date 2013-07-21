80af5fcdce1e4353e297ac6eaaaa48512c0b9a89
geekduck <duck1218+github@gmail.com>
2013-07-21 21:40:28 +0900

    Emacs用のファイルを削除. 代わりにsetup.sh内でEmacs用のリポジトリをクローンする.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=( .zshrc .zshrc.alias .zshrc.git .zshenv .zlogin .zshrc.completions .zshrc.antigen .gitconfig .tmux.conf .dir_colors .emacs.d .gemrc .git_templates)
+DOT_FILES=( .zshrc .zshrc.alias .zshrc.git .zshenv .zlogin .zshrc.completions .zshrc.antigen .gitconfig .tmux.conf .dir_colors .gemrc .git_templates)
 
 for file in ${DOT_FILES[@]}
 do
@@ -19,9 +19,11 @@ case "${OSTYPE}" in
     ;;
 esac
 
+# Emacs用リポジトリのクローン
+git clone git@github.com:def-my/emacs.d.git ~/.emacs.d
+
 # antigen.zshのダウンロード
-cd ~
-curl https://raw.github.com/zsh-users/antigen/master/antigen.zsh > .antigen.zsh
+curl https://raw.github.com/zsh-users/antigen/master/antigen.zsh > ~/.antigen.zsh
 
 # SHELL reload
 exec $SHELL -l
