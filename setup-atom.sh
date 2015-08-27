b6fe928cf43d598e23795fa239d34bfcef91c427
geekduck <duck1218+github@gmail.com>
2015-08-27 22:47:43 +0900

    Add packages.

--- setup-atom.sh
+++ setup-atom.sh
@@ -2,7 +2,12 @@
 
 apm install Sublime-Style-Column-Selection
 apm install atom-beautify
+apm install highlight-selected
 apm install minimap
+apm install minimap-find-and-replace
+apm install minimap-git-diff
+apm install minimap-highlight-selected
+apm install minimap-selection
 apm install linter
 apm install pigments
 apm install pretty-json
